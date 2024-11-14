3ec92fadee05f73da0b82a7a0a9acad8ae6bf80e
Kptkrunch <joshua.tylor.edwards@gmail.com>
2024-11-14 14:37:26 -0600

    Refactor asset sprites and add sorting scripts
    Renamed and reorganized sprite assets for better structure within the project. Updated project settings to enhance gizmo visibility and created new scripts for dynamic Y-axis sorting of objects and lights.

---
+++ Assets/Scripts/Objects/SortObjectByYAxis.cs
@@ -0,0 +1,32 @@
+using MoreMountains.Tools;
+using UnityEngine;
+
+namespace Objects
+{
+    public class SortObjectByYAxis : MonoBehaviour
+    {
+        public SpriteRenderer spriteRenderer;
+        public Transform objectCenterTransform;
+        public bool isStaticObject;
+        
+        private int _yAxis;
+
+        private void Awake()
+        {
+            _yAxis = (int)(objectCenterTransform.position.y * -10).RoundDown(0);
+            if (spriteRenderer)
+            {
+                spriteRenderer.sortingOrder = _yAxis;
+            } 
+            Debug.Log(_yAxis);
+        }
+
+        private void FixedUpdate()
+        {
+            if (isStaticObject) return;
+            _yAxis = (int)(objectCenterTransform.position.y * -10).RoundDown(0);
+            spriteRenderer.sortingOrder = _yAxis;
+            Debug.Log("inside the update y axis");
+        }
+    }
+}
