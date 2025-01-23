706d04e578ee58b6a0204a9f0e385f423b2e1c70
Kptkrunch <joshua.tylor.edwards@gmail.com>
2025-01-22 19:31:51 -0600

    VER-0.01.32: updated missile detonation

--- Assets/Scripts/Weapons/Projectiles/MeatSeeker.cs
+++ Assets/Scripts/Weapons/Projectiles/MeatSeeker.cs
@@ -12,12 +12,24 @@ namespace Weapons.Projectiles
         public float damage;
         public float blastRadius;
         public SplineAnimate animator;
-        public GameObject parent;
+        public GameObject parent, missileTrack;
 
         private void FixedUpdate()
         {
-            if (animator.isActiveAndEnabled && !animator.IsPlaying) parent.SetActive(false);
+            if (animator.isActiveAndEnabled && !animator.IsPlaying) Detonate();
         }
+
+        private void Detonate()
+        {
+            var exp = ProjectilePoolManager2.poolProj.projPools[pid].GetPooledGameObject();
+            var damager = exp.GetComponent<EExplosionDamager>();
+            damager.damage = damage;
+            damager.blastRadiusCollider.radius = blastRadius;
+            exp.gameObject.transform.position = transform.position;
+            exp.SetActive(true);
+            GenericShakeController.Instance.ShakeWeakStrongViolent("strong", transform);
+            animator.Restart(false);
+            missileTrack.SetActive(false); }
 
         private void OnDisable()
         {
@@ -29,6 +41,7 @@ namespace Weapons.Projectiles
             exp.SetActive(true);
             GenericShakeController.Instance.ShakeWeakStrongViolent("strong", transform);
             animator.Restart(false);
+            missileTrack.SetActive(false);
         }
     }
 }
